cd623900056df67b4febe8b84c9d796ff97d9c8f
limingxing <limingxing_aqgy@sina.com>
2016-02-23 13:16:42 +0800

    注释

--- HeadFirst/src/com/limx/factory/ChicagoStyleCheesePizza.java
+++ HeadFirst/src/com/limx/factory/ChicagoStyleCheesePizza.java
@@ -1,5 +1,11 @@
 package com.limx.factory;
-
+/**
+ * 具体芝加哥比萨子类
+ * @author limingxing
+ * @Date:2016-2-23下午1:13:25
+ * @email:limingxing_aqgy@sina.com
+ * @version:1.0
+ */
 public class ChicagoStyleCheesePizza extends Pizza{
 	public ChicagoStyleCheesePizza(){
 		name="Chicago Style Deep Dish Cheese Pizza";
